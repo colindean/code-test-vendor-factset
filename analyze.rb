b90e510c842b92240119c42c89ce4093f72a59c9
Colin Dean <git@cad.cx>
2019-02-06 00:21:05 -0500

    Rubocop autofix

--- analyze.rb
+++ analyze.rb
@@ -3,24 +3,24 @@ require 'csv'
 
 def clean_row(row)
   output = {}
-  row.to_hash.each_pair do |k,v|
+  row.to_hash.each_pair do |k, v|
     # clean whitepace
-    value = v.strip if !v.nil?
+    value = v.strip unless v.nil?
     # TODO: consider other cleaning, e.g. parse to Number, etc.
     output[k.to_sym] = value
   end
   output
 end
 
-geo_data = CSV.open(File.new("data/mdl__dim_geo.csv"), {headers: true}).pmap { |row| clean_row(row) }
-vendor_data = CSV.open(File.new("data/mdl__dim_vendor.csv"), {headers: true}).pmap { |row| clean_row(row) }
+geo_data = CSV.open(File.new('data/mdl__dim_geo.csv'), headers: true).pmap { |row| clean_row(row) }
+vendor_data = CSV.open(File.new('data/mdl__dim_vendor.csv'), headers: true).pmap { |row| clean_row(row) }
 
 geos_by_geo_id = geo_data.group_by { |geo| geo[:geo_id] }
 vendors_by_vendor_id = vendor_data.group_by { |vendor| vendor[:vendor_id] }
 
 Vendor = Struct.new(:id, :geo, :vendor) do
   include Comparable
-  def <=> other
+  def <=>(other)
     id <=> other.id
   end
 end
@@ -38,9 +38,9 @@ vendors = vendors_by_vendor_id.pmap do |id_vendor|
   end
 end
 
-factset_address_data = CSV.open(File.new("data/factset__ent_entity_address.csv"), {headers: true}).pmap { |row| clean_row(row) }
-factset_structure_data = CSV.open(File.new("data/factset__ent_entity_structure.csv"), {headers: true}).pmap { |row| clean_row(row) }
-factset_coverage_data = CSV.open(File.new("data/factset__ent_entity_coverage.csv"), {headers: true}).pmap { |row| clean_row(row) }
+factset_address_data = CSV.open(File.new('data/factset__ent_entity_address.csv'), headers: true).pmap { |row| clean_row(row) }
+factset_structure_data = CSV.open(File.new('data/factset__ent_entity_structure.csv'), headers: true).pmap { |row| clean_row(row) }
+factset_coverage_data = CSV.open(File.new('data/factset__ent_entity_coverage.csv'), headers: true).pmap { |row| clean_row(row) }
 
 factset_addresses_by_entity_id = factset_address_data.group_by { |thing| thing[:factset_entity_id] }
 factset_structure_by_entity_id = factset_structure_data.group_by { |thing| thing[:factset_entity_id] }
@@ -48,7 +48,7 @@ factset_coverage_by_entity_id  = factset_coverage_data.group_by { |thing| thing[
 
 Entity = Struct.new(:id, :address, :structure, :coverage) do
   include Comparable
-  def <=> other
+  def <=>(other)
     id <=> other.id
   end
 end
@@ -62,14 +62,13 @@ entities = factset_coverage_by_entity_id.pmap do |id_entity|
   entity
 end
 
-
 # Now, try to link a vendor to an entity.
 
 require 'jaro_winkler'
 
 NAME_CHECK_JARO_WINKLER_DISTANCE_THRESHOLD = 0.98
 DEBUG = false
-check_names = Proc.new do |entity, vendor|
+check_names = proc do |entity, vendor|
   output = []
   entity_names = [entity.coverage[:entity_name], entity.coverage[:entity_proper_name]]
   vendor_name = vendor.vendor[:name]
@@ -79,7 +78,7 @@ check_names = Proc.new do |entity, vendor|
   STDERR.puts "Is #{entity_names} in #{variants}?" if DEBUG
   entity_names.find do |name|
     if variants.include? name
-      hash = { factset_entity_id: entity.id, vendor_id: vendor.id, confidence: 1.0, reason: "name ~exact match" }
+      hash = { factset_entity_id: entity.id, vendor_id: vendor.id, confidence: 1.0, reason: 'name ~exact match' }
       STDERR.puts "EXACT MATCH! #{hash}"
       output << hash
     else
@@ -94,7 +93,6 @@ check_names = Proc.new do |entity, vendor|
   output
 end
 
-
 checks = [check_names]
 matches = []
 entities.each do |entity|
@@ -107,10 +105,8 @@ entities.each do |entity|
   end
 end
 
-CSV.open("matches.csv","w", {headers: matches.first.keys, write_headers: true}) do |csv|
+CSV.open('matches.csv', 'w', headers: matches.first.keys, write_headers: true) do |csv|
   matches.each do |match|
     csv << match.values
   end
 end
-
-
